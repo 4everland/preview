4c75780f5265b254702f627035915d8d6512419b
hqs <imhuangqs@gmail.com>
2023-09-26 16:01:43 +0800

    style: upate img bg

--- src/views/media/page-img.vue
+++ src/views/media/page-img.vue
@@ -8,7 +8,7 @@
   min-width: 50px;
   max-width: 90%;
   max-height: 95vh;
-  background: url(/imgs/bg-trans.svg);
+  background: #fff url(/imgs/bg-trans.svg);
   background-size: 30px;
   transition: all ease 200ms;
 }
